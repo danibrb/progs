fcd02f213ca1e0662a135a11743eae5506a9a7b8
danibrb <daniele.barbaro@calvino.edu.it>
2024-02-29 08:33:49 +0100

    boh

--- pico/blink.py
+++ pico/blink.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from machine import Pin
 from time import sleep
 
@@ -8,13 +7,3 @@ print('Blinking LED Example')
 while True:
   led.value(not led.value())
   sleep(0.5)
-=======
-import machine
-import utime
-led_onboard = machine.Pin(25, machine.Pin.OUT)
-while True:
-	led_onboard.value(1)
-	utime.sleep(1)
-	led_onboard.value(0)
-	utime.sleep(1)
->>>>>>> 1899d616e8ad7acba14c4ba92f9e852193411ed7
