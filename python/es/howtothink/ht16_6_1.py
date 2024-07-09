69576debd5e4ebb47184b14b9d240cad9a6f36c1
danibrb <daniele.barbaro@calvino.edu.it>
2024-07-09 10:11:18 +0200

    Sierpinski Triangle

---
+++ python/es/howtothink/ht16_6_1.py
@@ -0,0 +1,48 @@
+"""Sierpinski Triangle"""
+import turtle
+
+def drawTriangle(points,color,myTurtle):
+    """draw triangle"""
+    myTurtle.fillcolor(color)
+    myTurtle.up()
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.down()
+    myTurtle.begin_fill()
+    myTurtle.goto(points[1][0],points[1][1])
+    myTurtle.goto(points[2][0],points[2][1])
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.end_fill()
+
+def getMid(p1,p2):
+    """"get mid point"""
+    return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
+
+def sierpinski(points,degree,myTurtle):
+    """"def triangle"""
+    colormap = ['blue','red','green','white','yellow',
+                'violet','orange']
+    drawTriangle(points,colormap[degree],myTurtle)
+    if degree > 0:
+        sierpinski([points[0],
+                        getMid(points[0], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[1],
+                        getMid(points[0], points[1]),
+                        getMid(points[1], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[2],
+                        getMid(points[2], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+
+def main():
+    """"main"""
+    myTurtle = turtle.Turtle()
+    myTurtle.speed(0)
+    myWin = turtle.Screen()
+    myPoints = [[-200,-100],[0,200],[200,-100]]
+    sierpinski(myPoints,5,myTurtle)
+    myWin.exitonclick()
+
+main()
